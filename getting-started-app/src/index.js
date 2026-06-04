92b0a840f674fd49dd1071c51dd849dde4f6f120
udz1kor <nishanth.gundlapalle@in.bosch.com>
2026-06-04 15:55:53 +0530

    Add resource limits, morgan logging, .gitignore, and expand .dockerignore

--- getting-started-app/src/index.js
+++ getting-started-app/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const db = require('./persistence');
 const getItems = require('./routes/getItems');
@@ -6,6 +7,7 @@ const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
 
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
